a048d066e95502e22bc8bf7c0951c24dd19458f3
jjjuun-debug <sungjun9709@naver.com>
2025-07-04 23:32:46 +0900

    참고

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [제목, 제목변경] = useState('');
+  const [내용, 내용변경] = useState('');
+  const [날짜, 날짜변경] = useState('');
+  const [일기목록, 일기목록변경] = useState([]);
+
+  const 일기추가 = () => {
+    const 새일기 = {
+      id: Date.now(),
+      제목,
+      내용,
+      날짜
+    };
+
+    일기목록변경([...일기목록, 새일기]);
+    제목변경('');
+    내용변경('');
+    날짜변경('');
+  };
+
+  const 일기삭제 = (id) => {
+    일기목록변경(일기목록.filter((a) => a.id !== id));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ maxWidth: '600px', margin: '30px auto', padding: '20px', border: '1px solid #ccc'}}>
+      <h2> 일기장</h2>
 
-export default App
+      <input
+        type="text"
+        placeholder="제목"
+        value={제목}
+        onChange={(e) => 제목변경(e.target.value)}
+        style={{ width: '100%'}}
+      />
+      <textarea
+        placeholder="내용"
+        value={내용}
+        onChange={(e) => 내용변경(e.target.value)}
+        rows="4"
+        style={{ width: '100%'}}
+      />
+      <input
+        type="date"
+        value={날짜}
+        onChange={(e) => 날짜변경(e.target.value)}
+      />
+
+      <button onClick={일기추가}>일기 추가</button>
+
+
+      {일기목록.map((item) => (
+        <div key={item.id}>
+          <h4>{item.제목}</h4>
+          <p>{item.내용}</p>
+          <p>{item.날짜}</p>
+          <button onClick={() => 일기삭제(item.id)}>삭제</button>
+        </div>
+      ))}
+    </div>
+  );
+}
