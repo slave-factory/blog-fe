19052bc51727353629e16eb727c737cdb56b2aac
jjjuun-debug <sungjun9709@naver.com>
2025-07-06 23:49:03 +0900

    login-2

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,29 @@ export default function App() {
   if (창 === '홈') {
     return (
       <div>
-        <div className='header'>블로그<button onClick={()=> 창변경('로그인')}>로그인</button></div>
-        <div style={{ textAlign: 'center', marginTop: '100px' }}>
-          <button onClick={() => 창변경('로그인')} style={{ fontSize: '18px', padding: '10px 20px' }}>
+        <div className='header'>
+          <h4 onClick={()=>(창변경('홈'))} style={{cursor: 'pointer', marginLeft: '200px'}}>블로그</h4>
+          <button onClick={()=>(창변경('로그인'))} style={{marginLeft: '900px', cursor: 'pointer'}}>로그인</button>
+        </div>
+        <div style={{ 
+          textAlign: 'center', 
+          marginTop: '150px', 
+          marginLeft: '1050px',
+          border: '1px solid #ccc',
+          padding: '20px',
+          width: '220px',
+          height: '120px'
+          }}>
+          <button onClick={() => 창변경('로그인')} style={{ fontSize: '18px', padding: '10px 70px', marginTop: '10px' }}>
             로그인
           </button>
+          <div className="links">
+            <span>비밀번호 찾기</span>
+            <span>|</span>
+            <span>아이디 찾기</span>
+            <span>|</span>
+            <span>회원가입</span>
+          </div>
         </div>
       </div>
     );
@@ -29,8 +47,8 @@ export default function App() {
   if (창 === '로그인') {
     return (
       <div>
-        <div className='header'>블로그</div>
-        <div style={{ maxWidth: '300px', margin: '100px auto', textAlign: 'center' }}>
+        <div className='header'><h4 onClick={()=>(창변경('홈'))}  style={{cursor: 'pointer', marginLeft: '200px'}}>블로그</h4></div>
+        <div style={{ maxWidth: '300px', margin: '150px auto', textAlign: 'center' }}>
           <h2>로그인</h2>
           <input
             type="text"
@@ -38,6 +56,7 @@ export default function App() {
             value={아이디}
             onChange={(e) => 아이디변경(e.target.value)}
             style={{ width: '100%', marginBottom: '10px', padding: '8px' }}
+            onKeyDown={(e) => e.key === 'Enter' && 로그인()}
           />
           <input
             type="password"
@@ -45,6 +64,7 @@ export default function App() {
             value={비밀번호}
             onChange={(e) => 비밀번호변경(e.target.value)}
             style={{ width: '100%', marginBottom: '10px', padding: '8px' }}
+            onKeyDown={(e) => e.key === 'Enter' && 로그인()}
           />
           <button onClick={로그인} style={{ padding: '8px 16px', cursor: 'pointer' }}>
             로그인
